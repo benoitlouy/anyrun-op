24442de4e91ac2ae940e30245c04da30dab678ac
Benoit Louy <benoit.louy@fastmail.com>
2024-03-02 14:33:09 -0500

    update flake dependencies

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         {
           # declare projects
           # relPath is the relative path of a project to the flake root
-          nci.projects.${crateName}.relPath = "";
+          nci.projects.${crateName}.path = ./.;
           # configure crates
           nci.crates.${crateName} = {
             # export crate (packages and devshell) in flake outputs
